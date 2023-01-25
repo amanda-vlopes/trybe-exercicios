6e6924aaf199c85f1fafffddad417c7794972fe1
amanda-vlopes <amandavitor@hotmail.com>
2023-01-24 21:30:43 -0300

    Exercicios de fixação e exercicios do dia 2 da se ção 3

---
+++ fundamentos/secao-03/dia-02-javascript-array-e-loop_for/fixacao-arrays.js
@@ -0,0 +1,12 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu [1];
+
+// console.log(menuServices);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+menu.push ('Contato');
+console.log(menu);
